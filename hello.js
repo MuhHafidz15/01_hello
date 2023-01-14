688c191a83b8fdabebdc932e09e57da7d5be4c04
hikaaam <andrenuryana@gmail.com>
2023-01-14 21:09:54 +0700

    hello

---
+++ hello.js
@@ -0,0 +1,4 @@
+console.log("hello world"); //console gunanya untuk menampilkan di terminal
+console.log("halo dunia");
+
+console.log(124);
---
+++ typedata.js
@@ -0,0 +1,36 @@
+//type data yg sering gunakan ada 3:
+//string
+//number
+//array
+
+//1. string
+let contoh = "hafidz";
+//let digunakan untuk declarasi variable
+//variable adalah sebuah data yg memiliki nilai
+
+console.log("hello dunia"); //log string
+console.log(contoh);
+
+contoh = "ucup"; //merubah nilai variable "contoh" dari "hafidz" menjadi "ucup"
+console.log(contoh);
+
+//2. number
+let nomor = 10;
+console.log(nomor);
+nomor = 15;
+console.log(nomor);
+
+//3. array : array dimulai dari index ke 0
+let contohArray = [0, 288, 3, 90, 5, 6, 7, 8, 9]; //value
+
+console.log(contohArray);
+//mengambil array index ke 0
+console.log(contohArray[0]);
+
+//mengambil array index ke 5
+console.log(contohArray[5]);
+
+console.log(contohArray[3]);
+
+let contoh2 = [11, 12, 99, 65];
+console.log(contoh2);
